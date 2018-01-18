034898296cbb6d295b955228b50f32edeaa5bec0
Brett Morin <brettmorin2@gmail.com>
2018-01-18 10:05:11 -0800

    Hooked up cdoe for bower and Jquery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -3,5 +3,15 @@ var s = "";
 
 console.log("Hello Pluralsite");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function() {
+    console.log("You clicked on " + $(this).text());
+});
