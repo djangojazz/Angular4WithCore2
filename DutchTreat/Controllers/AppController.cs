9b9bcd4f9d3a84e55d9c2033a82d47db28b912d5
Brett Morin <brettmorin2@gmail.com>
2018-01-19 13:29:28 -0800

    Implemented Razor pages

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -14,12 +14,16 @@ namespace DutchTreat.Controllers
             return View();
         }
 
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
             ViewBag.Title = "Contact Us";
 
+            throw new InvalidOperationException("Bad things happen");
+
             return View();
         }
+
 
         public IActionResult About()
         {
