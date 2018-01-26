156cefa28b3b2d98a1c003b8b546b5ef382cf717
Brett Morin <brettmorin2@gmail.com>
2018-01-26 13:31:06 -0800

    Added creation of Entity and existing database as well

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -7,15 +7,30 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using DutchTreat.Services;
+using DutchTreat.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DutchTreat
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
+
             services.AddTransient<IMailService, NullMailService>();
             //Support for real mail service
 
