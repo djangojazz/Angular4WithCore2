61901f16fbaef31fa4a42b651368685e77ff106a
Brett Morin <brettmorin2@gmail.com>
2018-02-05 16:12:01 -0800

    Added API Controller

--- DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,27 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
-        public IEnumerable<Product> GetAllProducts() => _ctx.Products.OrderBy(x => x.Title).ToList();
+        public IEnumerable<Product> GetAllProducts()
+        {
+            try
+            {
+                _logger.LogInformation("Got all products");
+                return _ctx.Products.OrderBy(x => x.Title).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all the products: {ex}");
+                return null;
+            }
+        }
 
         public IEnumerable<Product> GetProductsByCategory(string category) => _ctx.Products.Where(x => x.Category == category).ToList();
 
